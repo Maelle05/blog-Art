49708a13bb15409fdf1b1b94259cafc44397fbc1
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-03-20 15:30:21 +0100

    :sparkles: ajout d images, d une feuille de style, et du lien vers la feuille de style dans toute les page, :lipstick: ajout du style sur la premiere page

--- index.php
+++ index.php
@@ -3,13 +3,85 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="style.css">
     <title>Gavé Bleu</title>
 </head>
 <body>
+    <div class="icon">
+        <div class="hamburger hamburger-middle"></div>
+    </div>
+
+    <section class="menu-container">
+        <div class="link-container-menu">
+            <a class="menu-container-link" href="index.html">ACCUEIL</a>
+            <a class="menu-container-link" href="articles.html">ARTICLES</a>
+            <a class="menu-container-link" href="about.html">A PROPOS</a>
+            <a class="menu-container-link" href="contact.html">CONTACT</a>
+            <a class="menu-container-link" href="connexion.html">CONNECTION</a>
+        </div>      
+    </section>
+
+    <section class="nav-bar">
+        <a class="no-display" href="index.html"><img class="logo-header" src="img/logo.png"></a>
+        <a href="articles.html">Articles</a>
+        <a href="about.html">A propos</a>
+        <a href="contact.html">Contact</a>
+        <a href="ConnectionUser.php">Connexion</a>
+    </section>
+
+    <section class="first-container">
+        <h1>Slogan</h1>
+    </section>
     <h1>Bienvenue sur Gavé Bleu</h1>
     <a href="ConnectionUser.php">Connexion</a>
     <a href="InscriptionUser.php">Inscription</a>
     <h2>Nos Articles</h2>
+
+    <div class="fixed-bar">
+        <div class="research-input-div">
+            <input class="research-input" type="research" placeholder="Rechercher" name="">
+            <img class="img-search" src="img/search.png">
+            <hr class="horizontal-bar">
+        </div>
+        <div class="check-input-div">
+            <h3>Trier par...</h3>
+            <div>
+                <label>
+                <input class="" type="checkbox" name="">
+                <span></span>
+                Nombre de like
+                </label>
+            </div>
+            <div>
+                <label>
+                <input class="" type="checkbox" name="">
+                Date de publication</label>
+            </div>
+            <hr class="horizontal-bar">
+        </div>
+
+        <div class="plus-vus-div">
+            <div class="les-plus-vus-container">
+                <h3>Les plus likés</h3>
+                <p><span class="big-number">1</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod… </p>
+                <hr class="horizontal-bar">
+                <p><span class="big-number">2</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod… </p>
+                <hr class="horizontal-bar">
+                <p><span class="big-number">3</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod… </p>
+            </div>
+            <hr class="horizontal-bar">
+        </div>
+
+        <div class="social-div">
+            <h3>Nos réseaux sociaux</h3>
+            <a href="#"><p><img class="logo-rs" src="img/insta.png"> gavebleu</p></a>
+            <a href="#"><p><img class="logo-rs" src="img/fb.png"> Gavé Bleu</p></a>
+
+            <hr class="horizontal-bar">
+        </div>
+    </div>
+
+    <section class="first-content-container">
 
     <?php  
         include "conect.php";
@@ -18,35 +90,41 @@
             ?>
 
                 <?php while($v = $Article->fetch()){ ?>
-                    <div>
-                        <?php
-                         $image = $v['UrlPhotA'];
-                         ?>
-                        <img src="<?= $v['UrlPhotA'] ?>"/>
-                        <h3>Article num <?= $v['NumArt']?> : <?= $v['LibTitrA']?></h3>
-                        <p><?= $v['LibChapoA']?></p>
-                        <p><?= $v['DtCreA']?></p>
-                        <p> <?= $v['Likes']?> likes</p>
-                        <a href="article.php?id=<?= $v['NumArt']?>">Lire -></a>
+                    <div class="photo-text-container">
+                        <div class="photo-container">
+                            <?php
+                             $image = $v['UrlPhotA'];
+                             ?>
+                            <img src="<?= $v['UrlPhotA'] ?>"/>
+                        </div>
+                        <div class="text-container">
+                            <h3>Article num <?= $v['NumArt']?> : <?= $v['LibTitrA']?></h3>
+                            <p><?= $v['LibChapoA']?></p>
+                            <p><?= $v['DtCreA']?></p>
+                            <p> <?= $v['Likes']?> likes</p>
+                        </div>
+                        <a class="lire-suite" href="article.php?id=<?= $v['NumArt']?>">Lire -></a>
                     </div>
                 <?php }
                 include "disconect.php";
                 ?>
+                <div class="all-article-container">
+                    <a class="all-article" href="">Voir tous les articles</a>
+                </div>
 
            <br />
                 <?php 
                     if(isset($erreur)){ echo $erreur;}
                 ?>
+    </section>
 
     <a href="mdp.php">Partie administration</a>
 
     <footer>
-        <p class="copyright" style="text-align: center;">
-            &copy; 2020 <span>Gavé Bleu</span>. All Rights Reserved.
-            <br>
-            <a href="https://icons8.com/icon/">Icons by Icons8</a>
-		  </p>
-    
+        <a href="#"><img class="logo-footer" src="img/logo.png"></a>
+        <a href="#">MENTIONS LEGALES</a>
+        <a href="#">COOKIES</a>
+        <a href="#">CHARTE DE MODERATION</a>
     </footer>
     
 </body>
