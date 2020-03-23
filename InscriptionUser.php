a46f5ba77c64e03d39d08013c043522886b58971
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-03-23 14:32:04 +0100

    :bug: ajout de lien correct

--- InscriptionUser.php
+++ InscriptionUser.php
@@ -92,19 +92,19 @@ function ctrlSaisies($saisie) {
     <section class="menu-container">
         <div class="link-container-menu">
             <a class="menu-container-link" href="index.php">ACCUEIL</a>
-            <a class="menu-container-link" href="articles.html">ARTICLES</a>
+            <a class="menu-container-link" href="index.php">ARTICLES</a>
             <a class="menu-container-link" href="about.html">A PROPOS</a>
             <a class="menu-container-link" href="contact.html">CONTACT</a>
-            <a class="menu-container-link" href="connexion.html">CONNECTION</a>
+            <a class="menu-container-link" href="ConnectionUser.php">CONNECTION</a>
         </div>      
     </section>
 
     <section class="nav-bar">
         <a class="no-display" href="index.php"><img class="logo-header" src="img/logo.png"></a>
-        <a href="articles.html">Articles</a>
+        <a href="index.php">Articles</a>
         <a href="about.html">A propos</a>
         <a href="contact.html">Contact</a>
-        <a class="active" href="connexion.html">Connection</a>
+        <a class="active" href="ConnectionUser.php">Connection</a>
     
     </section>
 
